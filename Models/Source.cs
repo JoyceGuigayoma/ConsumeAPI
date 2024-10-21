ee6223f192a4ea4d9616f833c3c64d158a5ed655
JoyceGuigayoma <joyceguigayoma@gmail.com>
2024-10-21 20:17:27 +0800

    joysss

---
+++ Models/Source.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityMembers.Models
+{
+    public class Source
+    {
+        public string Type { get; set; }
+        public string Text { get; set; }
+        public string RelatedType { get; set; }
+        public int RelatedId { get; set; }
+    }
+}
